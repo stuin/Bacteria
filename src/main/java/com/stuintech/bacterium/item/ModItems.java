256d066c5dda496eb7728321ec5cd4a649cff46f
stuin <stuin01@gmail.com>
2022-11-29 17:09:29 -0800

    - Updated to Minecraft 1.19.2 - Updated SocketWrench

--- src/main/java/com/stuintech/bacterium/item/ModItems.java
+++ src/main/java/com/stuintech/bacterium/item/ModItems.java
@@ -1,6 +1,8 @@
 package com.stuintech.bacterium.item;
 
 import com.stuintech.bacterium.Bacterium;
+import com.stuintech.socketwrench.SocketWrench;
+import com.stuintech.socketwrench.item.BasicWrenchItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.registry.Registry;
@@ -10,9 +12,15 @@ public class ModItems {
 
     public static Item jammer = new Jammer(settings.group(ItemGroup.REDSTONE));
     public static Item compactedMust = new Item(settings.group(ItemGroup.MATERIALS));
+
+    //Testing SocketWrench library
+    //public static BasicWrenchItem wrench = new BasicWrenchItem(settings.group(ItemGroup.TOOLS)
+    //        .maxCount(1).maxDamage(SocketWrench.DEFAULT_WRENCH_DURABILITY));
     
     public static void register() {
         Registry.register(Registry.ITEM, Bacterium.MODID + ":jammer", jammer);
         Registry.register(Registry.ITEM, Bacterium.MODID + ":compacted_must", compactedMust);
+
+        //Registry.register(Registry.ITEM, Bacterium.MODID + ":wrench", wrench);
     }
 }
